4e3600263e99bdaf2e9f94f11aa3a2c8ade655be
marco.fretz <marco.fretz@vshn.ch>
2018-01-02 10:03:01 +0100

    importer for ip-symcon

--- Vagrantfile
+++ Vagrantfile
@@ -2,4 +2,10 @@ Vagrant.configure("2") do |config|
   config.vm.box = "puppetlabs/ubuntu-16.04-64-nocm"
   config.vm.synced_folder "./", "/vagrant"
   config.vm.network "public_network", bridge: "en0: Wi-Fi (AirPort)"
+
+  config.vm.provider :virtualbox do |vb|
+    vb.gui  = false
+    vb.customize ['modifyvm', :id, '--memory', 2048]
+    vb.customize ['modifyvm', :id, '--cpus', 2]
+  end
 end
--- app.js
+++ app.js
@@ -10,13 +10,21 @@ var sources = config.sources
 function convert_value(value, dest) {
   switch (datapoints[dest].type) {
     case '4Byte':
-    var buf = new ArrayBuffer(4);
-    var view = new DataView(buf);
-    value.forEach(function (b, i) {
-      view.setUint8(i, b);
-    });
-    return view.getFloat32(0);
-    break;
+      var buf = new ArrayBuffer(4);
+      var view = new DataView(buf);
+      value.forEach(function (b, i) {
+        view.setUint8(i, b);
+      });
+      return view.getFloat32(0);
+      break;
+    case '4ByteUint':
+      var buf = new ArrayBuffer(4);
+      var view = new DataView(buf);
+      value.forEach(function (b, i) {
+        view.setUint8(i, b);
+      });
+      return view.getUint32(0);
+      break;
   }
 }
 
@@ -37,14 +45,20 @@ var connection = Knx.Connection({
       if (dest in datapoints) { // filter to only react on the dataponts we want
         var description = datapoints[dest].description
         var source_desc = sources[src]
-        console.log("%s **** KNX EVENT: %j, src: %j (%j), dest: %j, value: %j, description: %j",
+        if (datapoints[dest].name) {
+          var short_name = datapoints[dest].name
+        } else {
+          // remove common special chars to create a more machine readable tag
+          var short_name = datapoints[dest].description.replace(/([(,)])/g, '').replace(/([ ])/g, '_').replace(/[^\x00-\x7F]/g, '');
+        }
+        console.log("%s **** KNX EVENT: %j, src: %j (%j), dest: %j (%j), value: %j (%j), description: %j",
         new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
-        evt, src, source_desc, dest, convert_value(value, dest), description);
+        evt, src, source_desc, dest, short_name, convert_value(value, dest), value, description);
 
         influx.writePoints([
           {
             measurement: datapoints[dest].measurement,
-            tags: { group_addr: dest },
+            tags: { group_addr: dest, short_name: short_name },
             fields: { value: convert_value(value, dest) },
           }
         ])
